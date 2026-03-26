74726ad039c213bc343ebeb075263f9dc5db98ee
Saulo <22698083+saulodigital@users.noreply.github.com>
2026-03-26 02:50:42 +0000

    swizzle docroot

---
+++ src/theme/DocRoot/Layout/Sidebar/ExpandButton/index.tsx
@@ -0,0 +1,37 @@
+import React, {type ReactNode} from 'react';
+import {translate} from '@docusaurus/Translate';
+import IconArrow from '@theme/Icon/Arrow';
+import type {Props} from '@theme/DocRoot/Layout/Sidebar/ExpandButton';
+
+import styles from './styles.module.css';
+
+export default function DocRootLayoutSidebarExpandButton({
+  toggleSidebar,
+}: Props): ReactNode {
+  return (
+    <button
+      type="button"
+      className={styles.expandButton}
+      title={translate({
+        id: 'theme.docs.sidebar.expandButtonTitle',
+        message: 'Expand sidebar',
+        description:
+          'The ARIA label and title attribute for expand button of doc sidebar',
+      })}
+      aria-label={translate({
+        id: 'theme.docs.sidebar.expandButtonAriaLabel',
+        message: 'Expand sidebar',
+        description:
+          'The ARIA label and title attribute for expand button of doc sidebar',
+      })}
+      onClick={toggleSidebar}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          toggleSidebar();
+        }
+      }}>
+      <IconArrow className={styles.expandButtonIcon} />
+    </button>
+  );
+}
